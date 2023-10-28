a747081d0c7788cb22db35c4da68b8066c388806
max881230 <max.li@gogolook.com>
2023-10-29 01:40:51 +0800

    init

---
+++ myFirstRugPull/src/TradingCenterV2.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.17;
+
+// TODO: Try to implement TradingCenterV2 here
+contract TradingCenterV2 {
+    // 試圖在 test 中升級後 rug pull 搶走所有 user 的 usdc 和 usdt
+}
